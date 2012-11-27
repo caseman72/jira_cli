470b7c17862741c9954b954340903c52bb247aea
Casey Manion <CaseyM@marketleader.com>
2012-11-27 11:35:27 -0800

    adding mtbachelor.com to the mix

--- mtn.php
+++ mtn.php
@@ -1,5 +1,6 @@
 #!/usr/bin/php
 <?php
+
 /**
 * array_get value from array/object/et
 *
@@ -38,8 +39,26 @@ $color = array(
 	, 'RESET'   => "\033[39m"
 );
 
-# retrieve from website - mt. bachelor report - as JSON
-$json = `wget -q "http://forecast.weather.gov/MapClick.php?lat=43.98886243884903&lon=-121.68182373046875&site=pdt&smap=1&unit=0&lg=en&FcstType=json" -O -`;
+# from mtbachelor.com - printable report - depends on formatting - as HTML
+$html = `wget -q "http://www.mtbachelor.com/winter/mountain/snow_report/@@conditions.flyer.html" -O -`;
+$html = preg_replace("/[&]quot;/", '"', $html); // this results in bad HTML but ok with the find below
+$html = preg_replace("/Season Total /", "Season  ", $html); // aligns columns in output
+
+// find Snowfall conditions
+if (preg_match_all("/<th>([^<]* Snowfall)<\/th>.*?<td>([^<]+)<\/td>/s", $html, $conditions)) {
+	$n = count($conditions[0]);
+	$headings = $conditions[1];
+	$values = $conditions[2];
+
+	# output
+	print "{$color['BLACK']}http://www.mtbachelor.com/winter/mountain/snow_report/@@conditions.flyer.html{$color['RESET']}\n";
+	for($i=0; $i<$n; $i++) {
+		print "  {$headings[$i]} - {$color['RED']}{$values[$i]}{$color['RESET']}\n";
+	}
+}
+
+# from forecast.weather.gov - at lat/lon for mt. bachelor - as JSON
+$json = `wget -q "http://forecast.weather.gov/MapClick.php?lat=43.98886243884903&lon=-121.68182373046875&FcstType=json" -O -`;
 $obj = json_decode($json);
 
 # descriptions
@@ -52,12 +71,15 @@ $days = array_get('startPeriodName', $time, array());
 $n = count($days);
 
 # output
+print "\n\n{$color['BLACK']}http://forecast.weather.gov/MapClick.php?lat=43.98886243884903&lon=-121.68182373046875{$color['RESET']}\n\n";
 for($i=0; $i<$n; $i++) {
 	print "{$color['BLUE']}{$days[$i]}{$color['RESET']}\n";
 
 	$desc = trim($text[$i]);
 	$desc = preg_replace("/\.\s*/", ". ", $desc);
 	$desc = wordwrap($desc, 72);
-	$desc = preg_replace("/([Ss]now)/", $color['RED'] . "$1" . $color['RESET'], $desc);
-	print "{$desc}\n\n"; 
+	$desc = preg_replace("/([Ss]now)/", $color['RED'] ."$1". $color['RESET'], $desc);
+	$desc = preg_replace("/\n/", "\n  ", $desc);
+	print "  {$desc}\n\n"; 
 }
+
